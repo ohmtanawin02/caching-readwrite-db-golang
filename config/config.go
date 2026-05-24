1b785942962a4ce366fd8539af28f6b390e141f2
tanawin.jen <tanawin.jen@codelabdev.co>
2026-05-25 01:05:06 +0700

    config: add environment config with read/write DB and Redis

---
+++ config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+
+	"golang-fiber/pkg/database"
+)
+
+type Config struct {
+	Port        string `split_words:"true" default:"9392"`
+	Environment string `split_words:"true" default:"development"`
+	Debug       bool   `split_words:"true" default:"false"`
+
+	// Database
+	DBRead  database.DBConfig `split_words:"true"`
+	DBWrite database.DBConfig `split_words:"true"`
+
+	// Redis
+	RedisAddr     string `split_words:"true" default:"localhost:6380"`
+	RedisPassword string `split_words:"true" default:""`
+	RedisDB       int    `split_words:"true" default:"0"`
+}
+
+func Read() (*Config, error) {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Printf("warning: .env file not found, using system env")
+	}
+
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return nil, fmt.Errorf("error processing env config: %w", err)
+	}
+
+	return &cfg, nil
+}
